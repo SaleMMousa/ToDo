86358c408c46bec328aee144a3b0771618cbfd6b
salem abumousa <salem.000472@gmail.com>
2023-09-20 14:09:18 +0300

    Add Source Folder

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import InputCard from './components/InputCard/input';
+import TodoCard from './components/Todocard/todocard';
+
+function App() {
+  return (
+    <>
+      <InputCard />
+      <TodoCard />
+    </>
+    
+  );
+}
+
+export default App;
